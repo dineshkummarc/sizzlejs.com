75b37bbd595ef71bd355cd658a2dcb2128f4e6be
Richard D. Worth <rdworth@gmail.com>
2012-06-18 17:51:53 -0400

    Updated to latest grunt-wordpress and grunt-jquery-content

--- grunt.js
+++ grunt.js
@@ -1,17 +1,45 @@
 module.exports = function( grunt ) {
 
+grunt.loadNpmTasks( "grunt-clean" );
+grunt.loadNpmTasks( "grunt-html" );
 grunt.loadNpmTasks( "grunt-wordpress" );
+grunt.loadNpmTasks( "grunt-jquery-content" );
 
 grunt.initConfig({
+	clean: {
+		folder: "dist/"
+	},
+	htmllint: {
+		resources: "resources/*.html"
+	},
+	jshint: {
+		options: {
+			undef: true,
+			node: true
+		}
+	},
+	lint: {
+		grunt: "grunt.js"
+	},
+	watch: {
+		pages: {
+			files: "page/*.html",
+			tasks: "deploy"
+		}
+	},
+	"build-pages": {
+		all: grunt.file.expandFiles( "page/*" )
+	},
+	"build-resources": {
+		all: grunt.file.expandFiles( "resources/*" )
+	},
 	wordpress: grunt.utils._.extend({
-		dir: "dist"
+		dir: "dist/wordpress"
 	}, grunt.file.readJSON( "config.json" ) )
 });
 
-grunt.registerTask( "build-wordpress", function() {
-	grunt.file.copy( "index.html", "dist/posts/page/index.html" );
-});
-
-grunt.registerTask( "default", "build-wordpress" );
+grunt.registerTask( "default", "lint" );
+grunt.registerTask( "build-wordpress", "clean lint build-pages build-resources");
+grunt.registerTask( "deploy", "wordpress-deploy" );
 
 };
